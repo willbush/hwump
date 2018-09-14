c457d7648023d36fd1f3bb877f464ddeef5b210a
willbush <will.g.bush@gmail.com>
2018-09-14 02:07:11 -0500

    add some initial prototype code

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
-import Lib
+import Game
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "enter q to quit"
+  putStrLn "enter u to increment and d to decrement the player's room number."
+  loopGame mkInitialGame
+
+mkInitialGame :: Game
+mkInitialGame =
+  Game
+    { _player = Player {_playerRoom = 0, arrowCount = 10}
+    , _wumpus = Wumpus {_wumpusRoom = 0, isAsleep = True}
+    }
+
+loopGame :: Game -> IO ()
+loopGame g = do
+    print g
+    line <- getLine
+    print line
+    if line == "q"
+      then return ()
+      else loopGame $ updateGame g line
+
+updateGame :: Game -> String -> Game
+updateGame g "u" = mvPlayerUp g
+updateGame g "d" = mvPlayerDown g
+updateGame g _ = g
